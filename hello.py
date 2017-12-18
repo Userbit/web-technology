e2ba48a771b12c7437ea6bd6bedf18e2fafaf96d
Userbit <flyagin@gmail.com>
2017-12-18 21:06:47 +0000

    Before check of answer

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-bind = "0.0.0.0:8080"
+#bind = "0.0.0.0:8080"
 
 def app(environ, start_response):
-    data = ''
-    arr = environ["QUERY_STRING"].split("&");
-    for param in arr:
-        data += param + "\n"
+    #data = [i + '\n' for i in environ['QUERY_STRING'].split('&')]
+    data = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    #data = ''
+    #arr = environ["QUERY_STRING"].split("&");
+    #for param in arr:
+    #    data += param + "\n"
     #for key in environ:
         #data += key + "\n" #+ "\t:\t" + environ[key];
         #for val in environ[key]:
@@ -15,4 +17,4 @@ def app(environ, start_response):
         #('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return iter([data])
+    return data
--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf 
 sudo /etc/init.d/nginx restart
-sudo gunicorn -D -c /home/box/web/hello.py hello:app
+sudo gunicorn -c /home/box/web/hello.cfg hello:app
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
+#sudo ln -sf /home/box/web/hello.cfg /etc/gunicorn.d/hello.py
